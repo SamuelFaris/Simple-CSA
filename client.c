2d3133762b8829abd592d2809e7d938c85bc8336
SamuelFaris <samuel.faris@wsu.edu>
2023-10-15 04:27:33 -0700

    Separate msg/file threads, RDT for files

--- client.c
+++ client.c
@@ -11,123 +11,182 @@ CLIENT CODE FOR SIMPLE CSA PROGRAM SUPPORTING MESSAGING AND FILE TRANSFER
 
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/signal.h>
+#include "csa.h"
 
-#define PORT 1234               //Port number
-#define SERVER_IP "127.0.0.1"   //Localhost
-#define BUFFER_SIZE 1024
-int exit_flag = 0;              //Global flag for SIGINT
+int msg_client_socket;
+int file_client_socket;
+char msg_buf[BUFFER_SIZE];
+char file_buf[BUFFER_SIZE];
 
-//Sets exit flag when SIGINT
+int polling_user = 0;
+int exit_flag = 0;
+
+struct send_file_args {
+    char filepath[256];
+    char filename[64];
+    int client_socket;
+};
+
 void sigint_handler(int sig) {
+    send(msg_client_socket, "QUIT", 4, 0);
     exit_flag = 1;
+    fflush(stdin);
 }
 
-void cls(void) {
-    printf("\033[2J");
-    printf("\033[H");
+//Function for sending files to server
+void *send_file(void *arg) {
+    struct send_file_args args = *(struct send_file_args *)arg;
+    char filepath[128], filename[64];
+    strcpy(filepath, args.filepath);
+    strcpy(filename, args.filename);
+    int client_socket = args.client_socket;
+
+    int n;
+    bzero(file_buf, BUFFER_SIZE);
+
+    FILE *ifp = fopen(filepath, "r");
+    if (ifp == NULL) {
+        perror("Error opening file");
+        return NULL;
+    }
+
+    //Get file size
+    int prev = ftell(ifp);                  //save SOF
+    fseek(ifp, 0L, SEEK_END);               //go to EOF
+    size_t filesize = (size_t)ftell(ifp);   //get offset of EOF
+    fseek(ifp, prev, SEEK_SET);             //go back to SOF
+
+    //Construct and send metadata payload (name and size of file being sent)
+    char metadata[BUFFER_SIZE];
+    snprintf(metadata, sizeof(metadata), "FILE %s %zu", filename, filesize);
+    send(client_socket, metadata, BUFFER_SIZE, 0);
+
+    //Wait for ack
+    if(!wait_for_ack_nak(client_socket)) {
+        free(arg);
+        fclose(ifp);
+        printf("Error: could not send first packet, ending transmission\n");
+        return NULL;
+    }
+
+    int failed = 0;
+    size_t sent = 0;
+    char file_out[BUFFER_SIZE];
+    while (sent < filesize) {
+        //Calculate bytes to read + send
+        size_t remaining = filesize - sent;
+        size_t chunk_size = (remaining > BUFFER_SIZE) ? BUFFER_SIZE : remaining;
+
+        fread(file_out, 1, chunk_size, ifp);
+        n = send(client_socket, file_out, chunk_size, 0);
+        if (n < 0 || !wait_for_ack_nak(client_socket)) {
+            printf("Error: could not transmit packet\n");
+            failed = 1;
+            break;
+        }
+        sent += n;
+    }
+
+    //Wait for user to enter prompt and output status of transmit
+    char output[256];
+    if (!failed) {
+        sprintf(output, "File '%s' received by server\n", filename);
+    } else 
+        sprintf(output, "File '%s' not received by server\n", filename);
+    while(polling_user) {};
+    printf("%s", output);
+
+    free(arg);
+    fclose(ifp);
+
+    return NULL;
 }
 
 int main() {
 
-    cls();
-
-    //Register SIGINT handler
     signal(SIGINT, sigint_handler);
-
-    //Create client socket
-    int client_socket;
-    struct sockaddr_in server_addr;
+    cls(stdout);
+    
+    struct sockaddr_in msg_server_addr;
+    struct sockaddr_in file_server_addr;
     char buffer[BUFFER_SIZE];
     FILE *ifp;
+    pthread_t tid;
     int n;
-
-    client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket < 0) {
+    
+    //Init sockets
+    msg_client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (msg_client_socket < 0) {
         perror("Socket error");
         exit(1);
     }
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = PORT;
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    file_client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (file_client_socket < 0) {
+        perror("Socket error");
+        exit(1);
+    }
 
-    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) != 0) {
+    //Server addr settings
+    msg_server_addr.sin_family = AF_INET;
+    msg_server_addr.sin_port = MSG_PORT;
+    msg_server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    file_server_addr.sin_family = AF_INET;
+    file_server_addr.sin_port = FILE_PORT;
+    file_server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    //Attempt connection to localhost
+    if (connect(msg_client_socket, (struct sockaddr*)&msg_server_addr, sizeof(msg_server_addr)) != 0) {
         perror("Error in connection");
         exit(1);
     }
-    printf("Established connection with server.\n");
+    printf("Established messaging connection with server.\n");
 
+    if (connect(file_client_socket, (struct sockaddr*)&file_server_addr, sizeof(file_server_addr)) != 0) {
+        perror("Error in connection");
+        exit(1);
+    }
+    printf("Established file transfer connection with server.\n");
+
+    //Loop for sending user files/messages
     while (!exit_flag) {
+        //Get user command
+        polling_user = 1;
         printf("Enter message: ");
-        fgets(buffer, sizeof(buffer), stdin);
-        buffer[strcspn(buffer, "\n")] = '\0';  // Remove the newline character
+        fgets(buffer, BUFFER_SIZE, stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';
+        polling_user = 0;
 
         //File transfer
         if (strncmp("FILE ", buffer, 5) == 0) {
-            
-            //Open file on client side
-            char filepath[1024];
-            char filename[256];
-            strcpy(filepath, "client_files/");
-            strcpy(filename, buffer + 5);
-            strcat(filepath, filename);
+            memcpy(file_buf, buffer, BUFFER_SIZE);
 
-            ifp = fopen(filepath, "r");
-            if (ifp == NULL) {
-                perror("Error in opening file");
-                continue;
-            }
+            //Extract filepath info
+            struct send_file_args *thread_args = (struct send_file_args *)malloc(sizeof(struct send_file_args));
+            strncpy(thread_args->filename, buffer + 5, 64);
+            strcpy(thread_args->filepath, "client_files/");
+            strcat(thread_args->filepath, thread_args->filename);
+            thread_args->client_socket = file_client_socket;
 
-            //Get file size
-            int prev = ftell(ifp);          //save SOF
-            fseek(ifp, 0L, SEEK_END);       //go to EOF
-            size_t filesize = ftell(ifp);   //get offset of EOF
-            fseek(ifp, prev, SEEK_SET);     //go back to SOF
-
-            //Construct and send metadata payload (name and size of file being sent)
-            char metadata[BUFFER_SIZE];
-            snprintf(metadata, sizeof(metadata), "FILE %s %zu", filename, filesize);
-            send(client_socket, metadata, BUFFER_SIZE, 0);
-
-            //Send file
-            while ((n = fread(buffer, 1, sizeof(buffer), ifp)) > 0) {
-                send(client_socket, buffer, n, 0);
-            }
-            fclose(ifp);
-
-            //Wait for ACK from server
-            bzero(buffer, BUFFER_SIZE);
-            n = recv(client_socket, buffer, BUFFER_SIZE, 0);
-            if (n <= 0) {
-                perror("No ACK from server");
-                break;
-            }
-
-            if (strncmp("ACK", buffer, 3) == 0) {
-                printf("File '%s' sent and received by server\n", filename);
-            } else {
-                printf("ERROR: File '%s' sent but not received by server\n", filename);
-            }
-
+            //Create a thread to send file in background
+            pthread_t ftid;
+            pthread_create(&ftid, NULL, send_file, (void *)thread_args);
 
         //Stopping connection
         } else if (strncmp("QUIT", buffer, 4) == 0) {
-            send(client_socket, "QUIT", 4, 0);
-            cls();
+            send(msg_client_socket, "QUIT", 4, 0);
+            cls(stdout);
             break;
 
         //Sending messages
         } else {
-            send(client_socket, buffer, strlen(buffer), 0);
+            memcpy(msg_buf, buffer, BUFFER_SIZE);
+            send(msg_client_socket, msg_buf, BUFFER_SIZE, 0);
         }
     }
 
-    close(client_socket);
+    close(msg_client_socket);
+    close(file_client_socket);
     return 0;
 }
--- server.c
+++ server.c
@@ -1,117 +1,237 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/signal.h>
+/*
+SERVER CODE FOR SIMPLE CSA PROGRAM SUPPORTING MESSAGING AND FILE TRANSFER
 
-#define PORT 1234               //Port number
-#define BUFFER_SIZE 1024
+    -Saves transmitted files to server_files/
 
-void cls(void) {
-    printf("\033[2J");
-    printf("\033[H");
+*/
+#include "csa.h"
+
+int file_server_socket;
+int file_client_socket;
+char file_buf[BUFFER_SIZE];
+
+int msg_server_socket;
+int msg_client_socket;
+char msg_buf[BUFFER_SIZE];
+
+pthread_t msg_tid;
+pthread_t file_tid;
+
+int quit_flag = 0;
+int server_exit = 0;
+
+void sigint_handler(int sig) {
+    printf("\nShutting down server\n");
+
+    //Close sockets
+    close(msg_client_socket);
+    close(file_client_socket);
+    close(msg_server_socket);
+    close(file_server_socket);
+    printf("Cleanup complete, exiting\n");
+    exit(0);
+}
+
+//Function for handling messages in foreground. Returns 1 if client sent QUIT, 0 otherwise
+void *handle_messages(void *socket){
+    int client_socket = *(int *)socket;
+    char ack[128] = "";
+
+    //Make socket nonblocking
+    fcntl(client_socket, F_SETFL, fcntl(client_socket, F_GETFL, 0) | O_NONBLOCK);
+    
+    while(!server_exit) {
+        bzero(msg_buf, BUFFER_SIZE);
+        int n = recv(client_socket, msg_buf, BUFFER_SIZE, MSG_DONTWAIT);
+        if (n < 0) {
+            //No data case
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                usleep(10000);
+                continue;
+            //Error case
+            } else {
+                perror("Error in receiving message from server");
+                break;
+            }
+
+        //Client quitting
+        } else if (strncmp("QUIT", msg_buf, 4) == 0) {
+            printf("Client requested to quit. Closing connection.\n");
+            quit_flag = 1;
+            pthread_exit(0);
+
+        //Client sent message
+        } else if (n > 0) {
+            printf("Received message: %s\n", msg_buf);
+            strcpy(ack, "ACK MSG");
+            send(client_socket, ack, strlen(ack), 0);
+        }
+    }
+    pthread_exit(0);
+}
+
+void *handle_file_transfer(void* socket) {
+    int client_socket = *(int *)socket;
+
+    while(1) {
+        bzero(file_buf, BUFFER_SIZE);
+        int n = recv(client_socket, file_buf, BUFFER_SIZE, 0);
+        if (n < 0) {
+            perror("Error in receiving message from client");
+
+        //Client sending file
+        } else if (strncmp("FILE ", file_buf, 5) == 0) {
+            //Parse metadata
+            strtok(file_buf, " ");                  //'FILE'
+            char *filename = strtok(NULL, " ");     //filename
+            char *sizestr  = strtok(NULL, " ");     //filesize as string
+            size_t filesize = atol(sizestr);        //filesize as long
+
+            //Create and open new file for incoming data
+            char filepath[256];
+            char *filename_cpy = (char *)malloc(strlen(filename));
+            memcpy(filename_cpy, filename, strlen(filename));
+            strcpy(filepath, "server_files/");
+            strcat(filepath, filename);
+
+        
+            FILE *ofp = fopen(filepath, "w");
+            if (ofp == NULL) {
+                perror("Error in opening file");
+                exit(1);
+            }
+
+            size_t received = 0;
+            int iters = 0;
+
+            //ACK the initial RTS            
+            send_ack(client_socket);
+
+            while (1) {
+                size_t remaining = filesize - received;
+                size_t chunk_size = (remaining > BUFFER_SIZE) ? BUFFER_SIZE : remaining;
+
+                //Done when received = filesize
+                if (chunk_size <= 0) {
+                    printf("File '%s' received from client\n", filename_cpy);
+                    break;
+                }
+
+                //Else pull next [chunk_size] bytes from buffer
+                n = recv(client_socket, file_buf, chunk_size, 0);
+                if (n == -1) {
+                    printf("File '%s' transfer unsuccessful\n", filename_cpy);
+                    break;
+                }
+
+                //Read packet into ofp
+                fprintf(ofp, "%s", file_buf);
+                received += n;
+
+                //ACK next packet
+                send_ack(client_socket);
+            }
+
+            fclose(ofp);
+            free(filename_cpy);
+
+        //Unrecognizable input
+        } else {
+            continue;
+        }
+    }
 }
 
 int main() {
 
-    cls();
+    signal(SIGINT, sigint_handler);
+    if(access("server_files/", F_OK) == 0) {
+        system("exec rm -r server_files/*");        //clean out folder
+    }
+    cls(stdout);
 
-    //Create server socket and requester socket
-    int server_socket, new_socket;
-    struct sockaddr_in server_addr, new_addr;
+    struct sockaddr_in msg_server_addr, file_server_addr;
     socklen_t addr_size;
     char buffer[BUFFER_SIZE];
     FILE *ofp;
     int n;
+    pthread_t tid;
 
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket < 0) {
+    //Init sockets
+    msg_server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (msg_server_socket < 0) {
         perror("Socket error");
         exit(1);
     }
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = PORT;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+    file_server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (file_server_socket < 0) {
+        perror("Socket error");
+        exit(1);
+    }
 
-    //Bind server socket
-    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+    //Server addr settings
+    msg_server_addr.sin_family = AF_INET;
+    msg_server_addr.sin_port = MSG_PORT;
+    msg_server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    file_server_addr.sin_family = AF_INET;
+    file_server_addr.sin_port = FILE_PORT;
+    file_server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    //Bind server sockets
+    if (bind(msg_server_socket, (struct sockaddr*)&msg_server_addr, sizeof(msg_server_addr)) < 0) {
         perror("Error in binding");
         exit(1);
     }
 
-    //Listen for connection requests and handle communications with client
-    while (1) {
-
-        //Listen on socket, queue 1 connection request max
-        if (listen(server_socket, 1) == 0) {
-            printf("Listening...\n");
-        } else {
-            printf("Error in listening\n");
-        }
-
-        //Wait for connection
-        addr_size = sizeof(new_addr);
-        new_socket = accept(server_socket, (struct sockaddr*)&new_addr, &addr_size);
-        cls();
-        printf("Established connection with client\n");
-
-        while (1) {
-            bzero(buffer, BUFFER_SIZE);
-            n = recv(new_socket, buffer, BUFFER_SIZE, 0);
-            if (n <= 0) {
-                perror("Error in receiving");
-                break;
-            }
-
-            //Handling files
-            if (strncmp("FILE ", buffer, 5) == 0) {
-                
-                //Parse metadata
-                strtok(buffer, " ");                    //'FILE'
-                char *filename = strtok(NULL, " ");     //filename
-                char *sizestr  = strtok(NULL, " ");     //filesize as string
-                size_t filesize = atol(sizestr);        //filesize as long
-
-                //Create and open new file for incoming data
-                char filepath[1024];
-                strcpy(filepath, "server_files/");
-                strcat(filepath, filename);
-
-                ofp = fopen(filepath, "w");
-                if (ofp == NULL) {
-                    perror("Error in opening file");
-                    exit(1);
-                }
-
-                //Read into file from buffer while data is present
-                size_t received = 0;
-                while (received < filesize) {
-                    n = recv(new_socket, buffer, BUFFER_SIZE, 0);
-                    if (n <= 0) {
-                        break;
-                    }
-                    fprintf(ofp, "%s", buffer);
-                    received += n;
-                }
-                fclose(ofp);
-                printf("File received from client and written to '%s'\n", filepath);
-
-                //Send ACK to client
-                send(new_socket, "ACK", 3, 0);
-
-            //Handling messages
-            } else if (strncmp("QUIT", buffer, 4) == 0) {
-                printf("Client requested to quit. Closing connection.\n");
-                break;
-            } else {
-                printf("Received message: %s\n", buffer);
-            }
-        }
-        cls();
+    if (bind(file_server_socket, (struct sockaddr*)&file_server_addr, sizeof(file_server_addr)) < 0) {
+        perror("Error in binding");
+        exit(1);
     }
 
-    close(server_socket);
+    while (!server_exit) {
+
+        //Listen on sockets
+        cls(stdout);
+        if (listen(msg_server_socket, 1) == 0) {
+            printf("Listening on messaging port...\n");
+        } else {
+            printf("Listening error\n");
+        }
+
+        if (listen(file_server_socket, 1) == 0) {
+            printf("Listening on file transfer port ...\n");
+        } else {
+            printf("Listening error\n");
+        }
+
+        //Accept requests on these ports
+        msg_client_socket = accept(msg_server_socket, NULL, NULL);
+        file_client_socket = accept(file_server_socket, NULL, NULL);
+
+        cls(stdout);
+        printf("Established connection on messaging port\n");
+        printf("Established connection on file transfer port\n");
+
+        while (!quit_flag) {    
+
+            //Create separate threads for each port
+            pthread_create(&msg_tid, NULL, handle_messages, &msg_client_socket);
+            pthread_create(&file_tid, NULL, handle_file_transfer, &file_client_socket);
+
+            //Wait for msg child to finish (user entered 'QUIT')
+            pthread_join(msg_tid, NULL);
+            pthread_cancel(file_tid);
+
+            //Close sockets
+            close(msg_client_socket);
+            close(file_client_socket);
+        }
+        quit_flag = 0;
+    }
+
+    close(msg_server_socket);
+    close(file_server_socket);
     return 0;
 }
